e7155bad2938f7fb3b5c3da82c412ce92b486dd5
Natali <rinykia@gmail.com>
2015-09-10 17:03:02 +0300

    Create table Comments

--- app.rb
+++ app.rb
@@ -22,6 +22,14 @@ configure do
   	content TEXT
  )'
 
+  @db.execute 'CREATE TABLE IF NOT EXISTS
+  "Comments" 
+  (
+  	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    created_date DATE, 
+  	content TEXT, 
+  	post_id INTEGER
+ )'
 end
 
 get '/' do
